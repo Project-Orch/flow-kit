2f679b5eea6bf96b9d4af472236e4837b4dcd6bd
Futheme <kyoro_souza@hotmail.com>
2024-05-31 23:46:19 -0300

    ✨ add icon components

---
+++ src/assets/icons/components/TerminalWindow.tsx
@@ -0,0 +1,12 @@
+/* GENERATED FILE */
+import React, { forwardRef } from "react";
+import type { Icon } from "../types";
+import IconBase from "../base";
+import weights from "../defs/TerminalWindow";
+
+const I: Icon = forwardRef((props, ref) => (
+  <IconBase ref={ref} {...props} weights={weights} />
+));
+
+I.displayName = "TerminalWindow";
+export { I as TerminalWindow };
