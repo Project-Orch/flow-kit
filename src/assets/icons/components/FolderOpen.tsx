cf61bc0ee2accab4703622d4e8702c0a4c2c43e3
Futheme <kyoro_souza@hotmail.com>
2024-06-03 21:39:06 -0300

    🍱 new icons for navigation pack nodes

---
+++ src/assets/icons/components/FolderOpen.tsx
@@ -0,0 +1,12 @@
+/* GENERATED FILE */
+import React, { forwardRef } from "react";
+import type { Icon } from "../types";
+import IconBase from "../base";
+import weights from "../defs/FolderOpen";
+
+const I: Icon = forwardRef((props, ref) => (
+  <IconBase ref={ref} {...props} weights={weights} />
+));
+
+I.displayName = "FolderOpen";
+export { I as FolderOpen };
---
+++ src/assets/icons/components/Folders.tsx
@@ -0,0 +1,12 @@
+/* GENERATED FILE */
+import React, { forwardRef } from "react";
+import type { Icon } from "../types";
+import IconBase from "../base";
+import weights from "../defs/Folders";
+
+const I: Icon = forwardRef((props, ref) => (
+  <IconBase ref={ref} {...props} weights={weights} />
+));
+
+I.displayName = "Folders";
+export { I as Folders };
